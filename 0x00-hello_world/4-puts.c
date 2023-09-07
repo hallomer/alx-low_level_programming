a124230a26011506b6a3b81c2c7347b14e2cf8bc
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-07 16:59:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
-int main(void){
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints with puts function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
