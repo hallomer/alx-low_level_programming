f113467f7b3734f57a296c27dc15d92160532419
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-07 17:05:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main(void){
 
-    printf (
-    return (0);
+/**
+ * main - entry point 
+ * 
+ * description: a c programm that prints with printf function
+ *
+ * return: always 0 (success) 
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
 }
