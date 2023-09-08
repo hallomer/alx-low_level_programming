e99ba385584176d777bdd65b78aacdfe534ba40b
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-07 17:20:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
- * 
- * description: a c programm that prints with printf function
+ * main - Entry point
  *
- * return: always 0 (success) 
+ * Description: a c programm that prints with printf function
+ *
+ * Return: always 0 (success)
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
