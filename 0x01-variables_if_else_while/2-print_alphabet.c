24ab2b8d9a81586f51f9cd45d8ab4cbae9f072cd
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-08 19:38:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabet
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
