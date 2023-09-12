eaea6057fd97918968390f712e40cf0a88faeaa1
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-12 14:39:06 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
+#include "main.h"
 /**
- * main - A program that prints _putchar
+ * main - a program that prints _putchar
  *
- * Return: 0 (success)
+ * Return: 0
 */
-
-#include <stdio.h>
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <main:wq.h>
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	void print_alphabet(void);
+
+	printf
