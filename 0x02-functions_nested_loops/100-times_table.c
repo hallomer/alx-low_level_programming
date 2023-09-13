0c33ace3f6aefc5d50fb77c518fc301727286cd3
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-12 19:36:31 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,49 @@
 #include "main.h"
-
 /**
  * print_times_table - prints the n times table
  * @n: parameter
  *
  * Return: void
 */
-
 void print_times_table(int n)
 {
-	int prod, mult, num;
+	int z = 0, rw, cl;
 
-	if (n <= 15 && n >= 0)
+	if (n > 15 || n < 0)
+		return;
+	while (z <= n)
 	{
-		for (num = 0; num <= n; num++)
+		for (rw = 0; rw <= n; rw++)
 		{
-			_putchar(48);
-			for (mult = 1; mult <= n; mult++)
+			cl = z * rw;
+			if (cl > 99)
+			{
+				_putchar(cl / 100 + '0');
+				_putchar((cl / 10 % 10) + '0');
+				_putchar(cl % 10 + '0');
+			}
+			else if (cl > 9)
+			{
+				_putchar(' ');
+				_putchar(cl / 10 + '0');
+				_putchar(cl % 10 + '0');
+			}
+			else if (rw != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(cl + '0');
+			}
+			else
+				_putchar(cl + '0');
+
+			if (rw != n)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-				if (prod <= 9)
-				{
-					_putchar(' ');
-				}
-				if (prod <= 99)
-				{
-					_putchar (' ');
-				}
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + 48);
-					_putchar((prod / 10) % 10 + 48);
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + 48);
-					_putchar((prod % 10) + 48);
-				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
+		z++;
 	}
 }
