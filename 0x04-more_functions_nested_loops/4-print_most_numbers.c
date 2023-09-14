b14f066686eeecafe7f566fdcb3dd41675107309
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-14 14:06:11 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_most_numbers - prints the numbers, from 0 to 9 except 2 ,4
  *
@@ -7,14 +8,14 @@
 
 void print_most_numbers(void)
 {
-        int i;
+	int i;
 
-        for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
 	}
-        _putchar('\n');
+	_putchar('\n');
 }
