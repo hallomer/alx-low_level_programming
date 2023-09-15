a5324c5c29e6f940491759823cfe319cfaf7a68b
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-14 17:22:53 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints numbers from 1 to 100
+ * multiplies of 3 print Fizz instead
+ * multiplies of 5 print Buzz instaed
+ * multiplies of both print FizzBuzz instead
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf(" Fizz");
+		}
+		else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf(" Buzz");
+		}
+		else if ((i % 5 == 0) && (i % 3 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
