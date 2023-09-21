863513cd3cb61070d5143e8e58d1b9f145d6af33
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-09-21 01:26:41 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,6 +15,13 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
 		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
 			str[i - 1] == '\n' || str[i - 1] == ',' ||
 			str[i - 1] == ';' || str[i - 1] == '.' ||
