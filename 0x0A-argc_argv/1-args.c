e8040af8cf743b28832df3a7a0c3c3cfc5f0a827
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-10-02 16:45:10 -0700

    0 and 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - program that prints number of arguments passed into it
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	_putchar(argc + '0');
+	_putchar('\n');
+	return (0);
+}
