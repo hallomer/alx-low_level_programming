051a155e6407b28f0b79d832cf4ae3db0bb7361b
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-10-04 05:16:24 -0700

    advanced

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,21 +3,51 @@
 
 /**
  * *argstostr - concatenates all the arguments of the program
- * @ac: count
- * @av: vector
+ * @ac: argument count
+ * @av: argument vector
  *
- * Return: pointer or NULL if it fails
+ * Return: pointer to new string or NULL if it fails
 */
 
 char *argstostr(int ac, char **av)
 {
+	int i, j, length = 0, index = 0;
+	char *newstr;
 
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
 
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			length++;
+			j++;
+		}
+		length++;
+	}
 
+	newstr = malloc((length + 1) * sizeof(char));
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
 
-
-
-
-
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			newstr[index] = av[i][j];
+			index++;
+			j++;
+		}
+	newstr[index] = '\n';
+	index++;
+	}
+	newstr[index] = '\0';
 	return (newstr);
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,17 +2,92 @@
 #include <stdlib.h>
 
 /**
- * **strtow - splits a string into words
- * @str: string to be splited
+ * count_words - counts the number of words in a string
+ * @str: string
  *
- * Return: a pointer to an array of strings or NULL if it fails
- */
+ * Return: number of words
+*/
 
+int count_words(char *str)
+{
+	int count = 0, i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * allocate_words - allocates memory for the words array
+ * @str: string
+ * @word_count: number of words
+ *
+ * Return: allocated words array
+*/
+
+static char **allocate_words(char *str, int word_count)
+{
+	char **words;
+	int i, j, k, len;
+
+	words = malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ')
+		{
+			len = 0;
+			k = i;
+			while (str[k] != ' ' && str[k] != '\0')
+			{
+				len++;
+				k++;
+			}
+			words[j] = malloc((len + 1) * sizeof(char));
+			if (words[j] == NULL)
+			{
+				for (k = 0; k < j; k++)
+					free(words[k]);
+				free(words);
+				return (NULL);
+			}
+			for (k = 0; k < len; k++)
+				words[j][k] = str[i++];
+			words[j][k] = '\0';
+			j++;
+		}
+		else
+			i++;
+	}
+
+	words[j] = NULL;
+	return (words);
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string
+ *
+ * Return: pointer, or NULL if str is NULL or empty, or if it fails
+*/
 char **strtow(char *str)
 {
+	char **words;
+	int word_count;
 
+	if (str == NULL || *str == '\0')
+	return (NULL);
 
+	word_count = count_words(str);
+	words = allocate_words(str, word_count);
 
-
-
-
+	return (words);
+}
