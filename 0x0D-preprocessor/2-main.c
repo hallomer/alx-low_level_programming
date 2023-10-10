212a68ad8bf523cc92ca583a3685df63b8aad57e
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-10-10 15:51:08 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,19 +1,5 @@
 #include <stdio.h>
 
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 /**
  * main - prints the name of the file it was compiled from
  *
@@ -22,13 +8,6 @@ int _putchar(char c)
 
 int main(void)
 {
-	int i;
-	char *filename = __FILE__;
-
-	for (i = 0; filename[i] != '\0'; i++)
-	{
-		_putchar(filename[i]);
-	}
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
 }
