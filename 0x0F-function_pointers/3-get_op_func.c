3ffbf03e598f934f9ccade2b4c022f604c991a90
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-10-11 15:42:25 -0700

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,14 +18,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int j = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (j < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (s[0] == ops[j].op[0])
 		{
-			return (ops[i].f);
+			return (ops[j].f);
 		}
+		j++;
 	}
 	return (NULL);
 }
