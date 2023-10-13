fa4d22d42ae3c80c907dd50c5362f5d7e2f0b33d
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-10-12 18:34:04 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,56 +2,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-
-/**
- * print_char - Prints a character
- * @args: va_list argument
-*/
-void print_char(va_list args)
-{
-	char c = va_arg(args, int);
-
-	printf("%c", c);
-}
-
-/**
- * print_int - Prints an integer
- * @args: va_list argument
-*/
-void print_int(va_list args)
-{
-	int d = va_arg(args, int);
-
-	printf("%d", d);
-}
-
-/**
- * print_float - Prints a float
- * @args: va_list argument
-*/
-void print_float(va_list args)
-{
-	double f = va_arg(args, double);
-
-	printf("%f", f);
-}
-
-/**
- * print_string - Prints a string
- * @args: va_list argument
- *         if the string is NULL, prints "(nil)"
-*/
-void print_string(va_list args)
-{
-	char *s = va_arg(args, char *);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-	}
-	printf("%s", s);
-}
-
 /**
  * print_all - Prints anything
  * @format: A list of types of arguments
@@ -60,43 +10,43 @@ void print_string(va_list args)
 */
 void print_all(const char * const format, ...)
 {
-	int i = 0, args_n;
 	va_list args;
-	void (*print_func)(va_list);
+	char *s;
+	int i;
+	char c;
+	float f;
 
 	va_start(args, format);
-	while (format && format[i])
+
+	while (*format)
 	{
-		if (format[i] == 'c' || format[i] == 'i' ||
-				format[i] == 'f' || format[i] == 's')
-			args_n++;
-		i++;
-	}
-	i = 0;
-	while (format && format[i])
-	{
-		switch (format[i])
+		switch (*format)
 		{
 			case 'c':
-				print_func = print_char;
+				c = va_arg(args, int);
+				printf("%c", c);
 				break;
 			case 'i':
-				print_func = print_int;
+				i = va_arg(args, int);
+				printf("%d", i);
 				break;
 			case 'f':
-				print_func = print_float;
+				f = va_arg(args, double);
+				printf("%f", f);
 				break;
 			case 's':
-				print_func = print_string;
+				s = va_arg(args, char *);
+				if (str == NULL)
+					printf("nil");
+				printf("%s", s);
 				break;
 			default:
-				i++;
+				format++;
 				continue;
 		}
-		print_func(args);
-		if (i < args_n)
+		if (*(format + 1))
 			printf(", ");
-		i++;
+		format++;
 	}
 	printf("\n");
 	va_end(args);
