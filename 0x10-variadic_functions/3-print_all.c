b26ddd44e0bcbf2f9afaa321781e4d4b51be5e54
“Hiba” <hebaaltayeb2@icloud.comroot@89db2eb07e02:/alx-pre_course/ git config --global user.email hebaaltayeb2@icloud.com>
2023-10-12 19:18:45 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,6 +13,7 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *separator = "";
 	va_list args;
+	char *str;
 
 	va_start(args, format);
 
@@ -24,14 +25,20 @@ void print_all(const char * const format, ...)
 				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
-				printf("%s%i", separator, va_arg(args, int));
+				printf("%s%d", separator, va_arg(args, int));
 				break;
 			case 'f':
 				printf("%s%f", separator, va_arg(args, double));
 				break;
 			case 's':
-				printf("%s%s", separator, va_arg(args, char*));
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
 				break;
+			default:
+				i++;
+				continue;
 		}
 		separator = ", ";
 		i++;
