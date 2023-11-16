30acc4e08c53d4751b3be8459342c8636d8a8bcc
hallomer <hebaaltayeb2@icloud.com>
2023-11-15 16:33:56 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,6 +11,9 @@ int _sqrt(int n, int i);
 */
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return (-1);
+
 	return (_sqrt(n, 1));
 }
 
@@ -23,12 +26,18 @@ int _sqrt_recursion(int n)
 */
 int _sqrt(int n, int i)
 {
+	int next_num;
+
 	if (i * i == n)
 		return (i);
 
 	else if (i * i > n)
 		return (-1);
 
-	else
-		return (_sqrt(i + 1, n));
+	next_num = _sqrt(n, i + 1);
+
+	if (next_num > 0)
+		return (next_num);
+
+	return (i);
 }
