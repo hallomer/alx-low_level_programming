d20b63c7c8e00ede71f8e406881ec8fc749e3891
hallomer <hebaaltayeb2@icloud.com>
2023-11-21 04:01:23 -0800

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
+int count_words(char *str);
+char **allocate_words_array(int count);
+void free_words_array(char **words, int count);
+void extract_words(char *str, char **words);
 
 /**
  * strtow - splits a string into words
@@ -7,24 +13,98 @@
  *
  * Return: a pointer to an array of strings, or NULL if it fails
  */
-
 char **strtow(char *str)
 {
-	int i, j, count = 0, start = 0, end = 0;
 	char **words;
+	int word_count, onlySpaces = 1, i;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+		{
+			onlySpaces = 0;
+			break;
+		}
+	}
+
+	if (onlySpaces)
+		return (NULL);
+
+	word_count = count_words(str);
+	if (word_count == 0)
+		return (NULL);
+
+	words = allocate_words_array(word_count);
+	if (words == NULL)
+		return (NULL);
+
+	extract_words(str, words);
+
+	return (words);
+}
+
+/**
+ * count_words - counts the number of words in a string
+ * @str: input string
+ *
+ * Return: number of words
+ */
+int count_words(char *str)
+{
+	int count = 0, i;
+
 	for (i = 0; str[i]; i++)
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 			count++;
 	}
+
+	return (count);
+}
+
+/**
+ * allocate_words_array - allocates memory for the array of words
+ * @count: number of words
+ *
+ * Return: pointer to the allocated array
+ */
+char **allocate_words_array(int count)
+{
+	char **words;
+
 	words = (char **)malloc((count + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-	i = count = 0;
-	while (str[i])
+
+	return (words);
+}
+
+/**
+ * free_words_array - frees the memory allocated for the array of words
+ * @words: array of words
+ * @count: number of words
+ */
+void free_words_array(char **words, int count)
+{
+	for (int i = 0; i < count; i++)
+		free(words[i]);
+
+	free(words);
+}
+
+/**
+ * extract_words - extracts words from the input string
+ * @str: input string
+ * @words: array of words
+ */
+void extract_words(char *str, char **words)
+{
+	int count = 0, start = 0, end = 0, i, j;
+
+	for (i = 0; str[i]; i++)
 	{
 		if (str[i] != ' ')
 		{
@@ -32,22 +112,20 @@ char **strtow(char *str)
 			while (str[i] && str[i] != ' ')
 				i++;
 			end = i;
+
 			words[count] = (char *)malloc((end - start + 1) * sizeof(char));
 			if (words[count] == NULL)
 			{
-				for (j = 0; j < count; j++)
-					free(words[j]);
-				free(words);
-				return (NULL);
+				free_words_array(words, count);
+				return;
 			}
+
 			for (j = 0; j < end - start; j++)
 				words[count][j] = str[start + j];
 			words[count][j] = '\0';
 			count++;
 		}
-		else
-			i++;
 	}
+
 	words[count] = NULL;
-	return (words);
 }
