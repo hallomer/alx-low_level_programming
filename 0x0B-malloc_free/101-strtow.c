45d43d7f20bfafbba887c028e642042147387752
hallomer <hebaaltayeb2@icloud.com>
2023-11-21 03:43:34 -0800

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,46 +7,47 @@
  *
  * Return: a pointer to an array of strings, or NULL if it fails
  */
+
 char **strtow(char *str)
 {
+	int i, j, count = 0, start = 0, end = 0;
 	char **words;
-	int i, j = 0, k, word_count = 0, word_length = 0;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i]; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-			word_count++;
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			count++;
 	}
-	words = malloc(sizeof(char *) * (word_count + 1));
+	words = (char **)malloc((count + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	i = count = 0;
+	while (str[i])
 	{
 		if (str[i] != ' ')
 		{
-			word_length++;
-			if (str[i + 1] == ' ' || str[i + 1] == '\0')
+			start = i;
+			while (str[i] && str[i] != ' ')
+				i++;
+			end = i;
+			words[count] = (char *)malloc((end - start + 1) * sizeof(char));
+			if (words[count] == NULL)
 			{
-				words[j] = malloc((word_length + 1) * sizeof(char));
-				if (words[j] == NULL)
-				{
-					for (k = 0; k < j; k++)
-						free(words[k]);
-					free(words);
-					return (NULL);
-				}
-				for (k = 0; k < word_length; k++)
-					words[j][k] = str[i - word_length + k + 1];
-
-				words[j][word_length] = '\0';
-				word_length = 0;
-				j++;
+				for (j = 0; j < count; j++)
+					free(words[j]);
+				free(words);
+				return (NULL);
 			}
+			for (j = 0; j < end - start; j++)
+				words[count][j] = str[start + j];
+			words[count][j] = '\0';
+			count++;
 		}
+		else
+			i++;
 	}
-	words[word_count] = NULL;
+	words[count] = NULL;
 	return (words);
 }
