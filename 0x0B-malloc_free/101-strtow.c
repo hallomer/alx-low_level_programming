701797bf5d9b5b4683ca0cdb2ad99ac96b1fce55
hallomer <hebaaltayeb2@icloud.com>
2023-11-21 03:06:31 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * **strtow - splits a string into words
- * @str: string to be splited
+ * strtow - splits a string into words
+ * @str: string to be split
  *
- * Return: a pointer to an array ot strings, or NULL if it fails
-*/
+ * Return: a pointer to an array of strings, or NULL if it fails
+ */
 char **strtow(char *str)
 {
 	char **words;
@@ -17,14 +17,12 @@ char **strtow(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i] == '\0'))
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			word_count++;
 	}
-
 	words = malloc(sizeof(char *) * (word_count + 1));
 	if (words == NULL)
 		return (NULL);
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ')
@@ -34,19 +32,21 @@ char **strtow(char *str)
 			{
 				words[j] = malloc((word_length + 1) * sizeof(char));
 				if (words[j] == NULL)
-					return (NULL);
-
-				for (k = 0; k < word_length; k++)
 				{
-					words[j][k] = str[i - word_length + 1 + k];
+					for (k = 0; k < j; k++)
+						free(words[k]);
+					free(words);
+					return (NULL);
 				}
+				for (k = 0; k < word_length; k++)
+					words[j][k] = str[i - word_length + k + 1];
+
 				words[j][word_length] = '\0';
 				word_length = 0;
 				j++;
 			}
 		}
 	}
-
 	words[word_count] = NULL;
 	return (words);
 }
