4905ae871a7417bd549f918c7dbfe31be24b42c5
hallomer <hebaaltayeb2@icloud.com>
2023-11-23 12:59:33 -0800

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,8 +12,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
-	char *src, *dest;
-	unsigned int i, copy_size = old_size < new_size ? old_size : new_size;;
+	unsigned int i;
 
 	if (new_size == 0)
 	{
@@ -31,10 +30,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 
-	src = (char *)ptr;
-	dest = (char *)new_ptr;
-	for (i = 0; i < copy_size; i++)
-		dest[i] = src[i];
+	for (i = 0; i < old_size & i < new_size; i++)
+		new_ptr[i] = ((char *)ptr)[i];
 
 	free(ptr);
 
