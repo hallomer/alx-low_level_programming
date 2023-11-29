6f3e965989c6ff47f062edca7f354643fefaea95
hallomer <hebaaltayeb2@icloud.com>
2023-11-29 02:00:05 -0800

    halo

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer
+ * @array: array of integers to be searched
+ * @size: size of array
+ * @cmp: a pointer to a function
+ *
+ * Return: index to first match, or -1 if it fails
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
