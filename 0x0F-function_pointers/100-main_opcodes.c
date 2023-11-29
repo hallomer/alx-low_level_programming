330a6ec2c6bc1fecc40c4483ec179cbb6a1cca88
hallomer <hebaaltayeb2@icloud.com>
2023-11-29 05:52:15 -0800

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point, a program that prints the opcodes of its main function
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int num_of_bytes, i;
+	unsigned char *opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num_of_bytes = atoi(argv[1]);
+
+	if (num_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	opcode = (unsigned char *)main;
+
+	for (i = 0; i < num_of_bytes; i++)
+	{
+		printf("%02x", opcode[i]);
+	}
+
+	printf("\n");
+	return (0);
+}
