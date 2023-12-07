620fc66403a774021ebd07daf27764d730a9ba7a
hallomer <hebaaltayeb2@icloud.com>
2023-12-07 10:10:55 -0800

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,11 +3,12 @@
 void print_first(void) __attribute__((constructor));
 
 /**
- * print_first - Prints the required message before the main function is executed
+ * print_first - Prints before the main function is executed
  *
  * Return: nothing
  */
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
