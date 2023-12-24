10c2fe8c3a12905d01151783a25e1eada2364b53
hallomer <hebaaltayeb2@icloud.com>
2023-12-24 12:45:34 -0800

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 	}
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT |
-			O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
