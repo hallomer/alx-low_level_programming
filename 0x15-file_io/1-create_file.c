8dfabbf8f4caa3f235a038d100e67a790593da61
hallomer <hebaaltayeb2@icloud.com>
2023-12-24 12:30:05 -0800

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * create_file - Creates a file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
-*/
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor, len = 0, write_result;
@@ -13,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 
@@ -23,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 
 		write_result = write(file_descriptor, text_content, len);
-		if (write_result == -1 | write_result != len)
+		if (write_result == -1 || write_result != len)
 		{
 			close(file_descriptor);
 			return (-1);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -57,7 +57,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	file_to = open(argv[2], O_WRONLY | O_CREAT |
+			O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
