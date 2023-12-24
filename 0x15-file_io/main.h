665b978f086d1d853cad70e9ca08887d8158e243
hallomer <hebaaltayeb2@icloud.com>
2023-12-24 12:58:00 -0800

    3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
