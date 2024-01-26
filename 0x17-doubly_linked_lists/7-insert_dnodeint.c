03d35ba1bbe5a47a6c15c12b489f4dde0a86fc1e
Hiba <hebaaltayeb2@icloud.com>
2024-01-25 17:39:07 -0800

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,42 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	dlistint_t *current = *h;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node, *current = *h;
 
-	if (!new_node || !*h)
+	if (!current && idx != 0)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
 
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		if (*h)
-			(*h)->prev = new_node;
+		new_node->next = current;
+		new_node->prev = NULL;
+		if (current)
+			current->prev = new_node;
 		*h = new_node;
-		return (new_node);
 	}
-
-	while ((i < idx - 1) && current)
+	else
 	{
-		current = current->next;
-		i++;
+		while (idx--)
+		{
+			if (current->next == NULL && idx == 0)
+			{
+				return (add_dnodeint_end(&current, n));
+			}
+			if (current->next == NULL && idx != 0)
+				return (NULL);
+			current = current->next;
+		}
+		new_node->next = current;
+		new_node->prev = current->prev;
+		current->prev->next = new_node;
+		current->prev = new_node;
 	}
-
-	if (i != idx - 1)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = new_node;
-	new_node->prev = current;
-	current->next = new_node;
 
 	return (new_node);
 }
